73cd4dacbd301256092aeab4397f9b16283c1d28
Tasneem Fatima <tasneems2208@gmail.com>
2022-01-13 15:24:13 +0530

    adding star-match game

---
+++ src/components/PlayNumber.js
@@ -0,0 +1,21 @@
+import React from 'react'
+const colors = {
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+};
+const PlayNumber = (props) => {
+    return (
+        <button
+            className="number"
+            style={{ backgroundColor: colors[props.status] }}
+            onClick={() => props.onClick(props.number, props.status)}
+        >
+            {props.number}
+        </button>
+
+    )
+}
+
+export default PlayNumber
